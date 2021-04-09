b8ca8a6cbee05bee4ab66fe159e011c36ffb3b85
alicenychto <als847a443pttfq.dhg29780yz@gmail.com>
2021-04-10 00:34:23 +0200

    ci update

--- .space.kts
+++ .space.kts
@@ -1,17 +1,14 @@
 @file:DependsOn("org.antlr:antlr4:4.8")
 
 import org.antlr.v4.Tool
+import java.io.File
 
 job("Generate parser and recognizer and then build the project") {
-    container(displayName = "generate files", "openjdk:11") {
-        kotlinScript {
+    container("openjdk:11") {
+        kotlinScript { api ->
             Tool.main(arrayOf("-o", "gen", "-visitor", "-no-listener", "src/main/amatsukazeGrammar.g4"))
             println("Grammar Recognizer generated.")
-        }
-    }
-    gradlew("openjdk:11", "build")
-    container("openjdk:11") {
-        kotlinScript {
+            api.gradlew("build")
             println("Build succeeded.")
         }
     }
