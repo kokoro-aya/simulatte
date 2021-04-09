a56e31874f3913ee32402a928c84d06e3ddac25f
kokoro aya <als847a443pttfq.dhg29780yz@gmail.com>
2021-04-09 22:02:16 +0000

    test - this should work

--- .space.kts
+++ .space.kts
@@ -3,11 +3,16 @@
 import org.antlr.v4.Tool
 
 job("Generate parser and recognizer and then build the project") {
-    /* container(displayName = "generate files", "openjdk:11") {
+    container(displayName = "generate files", "openjdk:11") {
         kotlinScript {
             Tool.main(arrayOf("-o", "gen", "-visitor", "-no-listener", "src/main/amatsukazeGrammar.g4"))
             println("Grammar Recognizer generated.")
         }
-    } */
+    }
     gradlew("openjdk:11", "build")
+    container("openjdk:11") {
+        kotlinScript {
+            println("Build succeeded.")
+        }
+    }
 }
