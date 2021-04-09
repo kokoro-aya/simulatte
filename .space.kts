59599ac2d2d142aad3a741cfa0f7d58fbe3d5b0d
kokoro-aya <als847a443pttfq.dhg29780yz@gmail.com>
2021-04-09 23:44:20 +0200

    fixed error

--- .space.kts
+++ .space.kts
@@ -3,19 +3,11 @@
 import org.antlr.v4.Tool
 
 job("Generate parser and recognizer and then build the project") {
-    parallel {
-        /* container(displayName = "generate files", "openjdk:11") {
-            kotlinScript {
-                Tool.main(arrayOf("-o", "gen", "-visitor", "-no-listener", "src/main/amatsukazeGrammar.g4"))
-                println("Grammar Recognizer generated.")
-            }
-        } */
-        container(displayName = "setup gradle wrapper", image = "ubuntu") {
-            shellScript {
-                interpreter = "/bin/bash"
-                content = "gradle wrapper"
-            }
+    /* container(displayName = "generate files", "openjdk:11") {
+        kotlinScript {
+            Tool.main(arrayOf("-o", "gen", "-visitor", "-no-listener", "src/main/amatsukazeGrammar.g4"))
+            println("Grammar Recognizer generated.")
         }
-    }
+    } */
     gradlew("openjdk:11", "build")
 }
